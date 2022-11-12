42dfada31871b46c6d39219d049825b051d0ad3d
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-12 19:37:17 +0530

    dangerouslySetInnerHTML

--- src/components/single-news/single-news-header/index.tsx
+++ src/components/single-news/single-news-header/index.tsx
@@ -7,8 +7,13 @@ const NewsHeader = (props: {
 }) => {
   return (
     <div className="flex flex-col">
-      <h1 className="font-serif font-bold text-2xl">{props?.description}</h1>
-      <p className="my-3 text-zinc-400" data-testid="paragraph">Updated At: Today, {props?.publishedAt}</p>
+      <h1
+        className="font-serif font-bold text-2xl"
+        dangerouslySetInnerHTML={{ __html: props?.description }}
+      ></h1>
+      <p className="my-3 text-zinc-400" data-testid="paragraph">
+        Updated At: Today, {props?.publishedAt}
+      </p>
     </div>
   );
 };

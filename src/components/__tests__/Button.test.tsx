2b81aad1bba384cce4cba29cca71623daca06494
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-13 20:02:57 +0530

    Button tests updated

--- src/components/__tests__/Button.test.tsx
+++ src/components/__tests__/Button.test.tsx
@@ -2,18 +2,25 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Button from "../button";
 
-describe("Button", () => {
-  it("Props passed to Button component should work as expected", async () => {
+describe("Button Tests", () => {
+  it("Renders a button with passed label", async () => {
     render(<Button label="Click Me" classes="myClass" onclick={() => {}} />);
 
     const clickMeButton = screen.getByRole("button", { name: "Click Me" });
-
     expect(clickMeButton).toBeInTheDocument();
+  });
 
-    //Checking class
+  it("Renders a button with passed classes", async () => {
+    render(<Button label="Click Me" classes="myClass" onclick={() => {}} />);
+
+    const clickMeButton = screen.getByRole("button", { name: "Click Me" });
     expect(clickMeButton).toHaveClass("myClass");
+  });
 
-    //Checking onClick
+  it("Renders a button with passed onClick function", async () => {
+    render(<Button label="Click Me" classes="myClass" onclick={() => {}} />);
+
+    const clickMeButton = screen.getByRole("button", { name: "Click Me" });
     clickMeButton.getAttribute("onClick") === "() => {}";
   });
 });

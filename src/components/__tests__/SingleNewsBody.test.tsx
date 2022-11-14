2dda38e924c096e58c5b06e79af7c076e2e4d38c
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-14 09:37:48 +0530

    single news body tests updated

--- src/components/__tests__/SingleNewsBody.test.tsx
+++ src/components/__tests__/SingleNewsBody.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import NewsBody from "../single-news/single-news-body";
 
-describe("NewsBody", () => {
-  it("Props passed to NewsBody component should work as expected", async () => {
+describe("Single NewsBody Tests", () => {
+  it("Renders a image with passed src", async () => {
     render(
       <NewsBody
         content="This is trial content"
@@ -13,14 +13,44 @@ describe("NewsBody", () => {
       />
     );
 
-    //image testing
     const image = screen.getByTestId("image");
     image.getAttribute("src") === "/news-body";
+  });
+
+  it("Renders a title with passed title text", async () => {
+    render(
+      <NewsBody
+        content="This is trial content"
+        title="myTitle"
+        image="/news-body"
+        url="/read-more"
+      />
+    );
 
     expect(screen.getByTestId("paragraph1").textContent).toEqual("myTitle");
+  });
 
-    expect(screen.getByTestId("paragraph2").textContent).toEqual(
-      "T..."
+  it("Renders sliced content of the news body with passed content", async () => {
+    render(
+      <NewsBody
+        content="This is trial content"
+        title="myTitle"
+        image="/news-body"
+        url="/read-more"
+      />
+    );
+
+    expect(screen.getByTestId("paragraph2").textContent).toEqual("T...");
+  });
+
+  it("Renders a link with 'Read More' label and provided href", async () => {
+    render(
+      <NewsBody
+        content="This is trial content"
+        title="myTitle"
+        image="/news-body"
+        url="/read-more"
+      />
     );
 
     const readMoreLink = screen.getByRole("link", { name: /Read More/i });

3f4b74209be935fc9512bcaa04114bc14da2a3a9
Subhash Jadhav <subhash.jadhav@bluepineapple.io>
2022-11-14 09:49:09 +0530

    Source card tests updated

--- src/components/__tests__/SourceCard.test.tsx
+++ src/components/__tests__/SourceCard.test.tsx
@@ -12,16 +12,24 @@ const sourceData = {
   country: "India",
 };
 
-describe("SourceCard", () => {
-  it("Props passed to SourceCard component should work as expected", async () => {
+describe("SourceCard Tests", () => {
+  it("Renders name of the source with passed name", async () => {
     render(<SourceCard source={sourceData} />);
 
     const sourceName = screen.getByRole("heading", { name: "The Hindu" });
     expect(sourceName).toBeInTheDocument();
+  });
+
+  it("Renders description of the source with passed description", async () => {
+    render(<SourceCard source={sourceData} />);
 
     expect(screen.getByTestId("paragraph").textContent).toEqual(
       "Unit testing with The Hindu..."
     );
+  });
+
+  it("Renders a link with 'Read More' label and passed href", async () => {
+    render(<SourceCard source={sourceData} />);
 
     const readMoreLink = screen.getByRole("link", { name: /Read More/i });
     expect(readMoreLink).toHaveAttribute("href", "/the-hindu");
